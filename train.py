a64237dd9b82faa029369e9121b4cb1c7a5d6d34
Nitin Kshatriya <nitinkshatriya29@gmail.com>
2018-08-29 01:40:49 +0530

    ROI extraction for Training Data

--- train.py
+++ train.py
@@ -1,15 +1,16 @@
 import os
 import cv2
 import numpy as np
-import faces
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "data")
 
-haar_face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 
 x_train = []
 y_labels = []
+start_label = 0
+label_id = {}
 
 for root, dirs, files in os.walk(image_dir):
 	for file in files:
@@ -17,3 +18,20 @@ for root, dirs, files in os.walk(image_dir):
 			path = os.path.join(root, file)
 			label = os.path.basename(root)
 			image = cv2.imread(path)
+			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+			
+			# Detecting Face and Creating Training Data
+			faces = cascade.detectMultiScale(gray, scaleFactor=1.2)
+			for (x, y, w, h) in faces:
+				face = gray[y:y+h, x:x+w]
+				x_train.append(face)
+
+			# Labels
+			if label not in label_id:
+				label_id[label] = start_label
+				start_label += 1
+			id = label_id[label]
+			y_labels.append(id)
+
+print(x_train, y_labels)
+print(label_id)
